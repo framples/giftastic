846d32a24d4d104a29f1cb65a8f3fa8f5d405296
Illio, the Great <pwfcoding@gmail.com>
2019-08-05 19:22:31 -0500

    starting code to make gifs pause

--- js.js
+++ js.js
@@ -101,5 +101,22 @@
     renderButtons();
 
 
-
+    $(".animalImage").on("click", function() {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        var dataState 
+        var dataAnimate = results[i].images.fixed_height.url;
+        var dataStill = results[i].images.fixed_height_still.url;   
+        
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
 
