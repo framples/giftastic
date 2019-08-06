140b6d869cf597dfb475b98723e5c1c8daeea50e
Illio, the Great <pwfcoding@gmail.com>
2019-08-06 11:48:06 -0500

    typing error updates

--- js.js
+++ js.js
@@ -22,31 +22,47 @@ function displayAnimalGif() {
 
             var rating = results[i].rating;
             console.log(rating);
-            
+
             var pOne = $("<p>").text("Rating: " + rating);
 
             var animalImage = $("<img>");
             animalImage.attr("src", results[i].images.fixed_height.url);
+           // animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+           // animalImage.attr("data-animate", results[i].images.fixed_height.url);
+           // animalImage.attr("data-state", "still");
+           // animalImage.attr("class", "gif");
 
-            // Appending the image
             animalDiv.append(animalImage);
             animalDiv.append(pOne);
 
             $("#animal-view").prepend(animalDiv);
 
+          //  function changeState(){
+          //      var state = $(this).attr("data-state");
+           //     var animateImage = $(this).attr("data-animate");
+          //      var stillImage = $(this).attr("data-still");
+            
+          //      if (state == "still") {
+          //          $(this).attr("src", animateImage);
+         //           $(this).attr("data-state", "animate");
+         //       }
+            
+          //      else if (state == "animate") {
+          //          $(this).attr("src", stillImage);
+           //         $(this).attr("data-state", "still");
+           //     }
+          //  }
+           // $(document).on("click", ".gif", changeState);
 
         }
     });
 
 }
 
-// Function for displaying movie data
-function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
+function renderButtons() {
     $("#buttons-view").empty();
-   
+
     for (var i = 0; i < animals.length; i++) {
 
         var a = $("<button>");
@@ -65,43 +81,23 @@ function clear() {
     $("#animal-view").empty();
 }
 
-// This function handles events where a movie button is clicked
+
 $("#add-animal").on("click", function (event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
-    var animal = $("#animal-input").val().trim();
 
-    // Adding movie from the textbox to our array
+    var animal = $("#animal-input").val().trim();
     animals.push(animal);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "movie-btn"
+
 $(document).on("click", ".animal-btn", displayAnimalGif);
 $(document).on("click", ".animal-btn", clear);
 
-// Calling the renderButtons function to display the intial buttons
 renderButtons();
 
 
-$(".animalImage").on("click", function () {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-    var dataState = "still";
-    var dataAnimate = results[i].images.fixed_height.url;
-    var dataStill = results[i].images.fixed_height_still.url;
 
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if (state === dataStill) {
-        $(this).attr("src", $(this).attr(dataAnimate));
-        $(this).attr(dataState, "animate");
-    } else {
-        $(this).attr("src", $(this).attr(dataStill));
-        $(this).attr(dataState, "still");
-    }
-});
+
 
